d4acc523435ffc864ac1c5b97284b33616d04292
MLoretto <loreto.henriquez.m@gmail.com>
2018-05-24 17:08:17 -0400

    Se modifica HTML, se agrega en CSS estilos a botones, realizo en ciper.js UNCODE y DECODE, el cual solo funciona con MAYUSCULAS y reconoce espacios. Funciona al ejecutar en navegador, y se ejecuta test sin error.

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,47 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (message, offSet ) => {
+    let result = "";
+    //repite el codigo caracter por caracter del mensaje.
+    for(let index=0;index<message.length;index++){
+      //toma el caracter por el indice INDEX y lo convierte en codigo ascii
+      const code = message.charAt(index).charCodeAt();
+      //valida que el rango sea un caracter mayuscula
+      if(code > 64 && code < 91){
+        var newCode = code + parseInt(offSet);
+        //mientras sea mayor a 90 va restando 26 (cantidad de letras del alfabeto)
+        while(newCode > 90){
+          newCode -= 26;
+        }
+      }else{
+        //por mientras ~,para ver que hago con caracteres distintos.
+        newCode = 126;
+      }
+      result += String.fromCharCode(newCode);
+    }
+
+	  return result;	
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (criptoMessage,offSet) => {
+    let result = "";
+    for(let index=0;index<criptoMessage.length;index++){
+      const code = criptoMessage.charAt(index).charCodeAt();
+      //quiere decir que es una mayuscula
+      if(code > 64 && code < 91){
+        var newOffSet = parseInt(offSet);
+        var newCode = code - newOffSet;
+        while(newCode < 65){
+          //calcula el desface con respecto al principio del alfabedo para descontar despues al final.
+          newCode =  (newCode - 64) + 90;
+          newOffSet = newOffSet - 90;
+        }
+        result += String.fromCharCode(newCode);
+      }else if(code == 126){
+        result += " ";
+      }else{
+        //por mientras ~,para ver que hago con caracteres distintos.
+        code = 126;
+      }
+    }
+    return result;	
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-function clicTrasform(){
-    console.log(document.getElementById("getMessage").value);
-}
+function clickCifrar(){
+    //obtengo el offset
+    var offSet = document.getElementById("offSetGo").value;
+    //obtengo el mensaje
+    var message =  document.getElementById("getMessage").value;
+    //Utilizo la libreria cipher para codificar el mensaje
+    var messageEncode = cipher.encode(message,offSet);
+    //El mensaje encriptado lo muestro en el elemento "showMessage"
+    document.getElementById("showMessage").value = messageEncode;
+
+  }
+  
+  function clickDecifrar(){
+    //obtengo el offset
+    var offSet = document.getElementById("offSetGo").value;
+    //obtengo el mensaje
+    var message =  document.getElementById("showMessage").value;
+    //utilizo la libreria cipher para decodificar el mensaje
+    var messageDecode = cipher.decode(message,offSet);
+    //El mensaje encriptado lo muestro en el elemento "getMessage"
+    document.getElementById("getMessage").value = messageDecode;
+  
+  }
+  
