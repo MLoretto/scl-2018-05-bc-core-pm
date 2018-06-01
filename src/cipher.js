c681c569ce97d186337115fde3d95ad95ad1774c
Loreto <loreto.henriquez.m@gmail.com>
2018-06-01 02:43:04 -0400

    Se actualiza nueva lógica de desarrollo de cipher, se modifica HTML, Estilos CSS, se agrega ayuda a usuario

--- src/cipher.js
+++ src/cipher.js
@@ -1,132 +1,42 @@
-/*
-window.cipher = {
-  encode: (message, offSet ) => {
-    let result = "";
-    //repite el codigo caracter por caracter del mensaje.
-    for(let index=0;index<message.length;index++){
-      //toma el caracter por el indice INDEX y lo convierte en codigo ascii
-      const code = message.charAt(index).charCodeAt();
-      //valida que el rango sea un caracter mayuscula
-      if(code > 64 && code < 91){
-        var newCode = code + parseInt(offSet);//parseInt asegurar que sea número, lo convierto a num(LMS)
-        //mientras sea mayor a 90 va restando 26 (cantidad de letras del alfabeto)
-        while(newCode > 90){
-          newCode -= 26; // El 26 le descuenta un alfabeto completo
-        }
-      }else if(code > 96 && code < 123){
-        var newCode = code + parseInt(offSet);//parseInt asegurar que sea número, lo convierto a num(LMS)
-        //mientras sea mayor a 122 va restando 26 (cantidad de letras del alfabeto)
-        while(newCode > 122){
-          newCode -= 26; // El 26 le descuenta un alfabeto completo
-        }
-      }else{
-        //por mientras ~,para ver que hago con caracteres distintos.
-        newCode = 126;
-      }
-      result += String.fromCharCode(newCode);
-    }
-
-	  return result;	
-  },
-  decode: (criptoMessage,offSet) => {
-    let result = "";
-    for(let index=0;index<criptoMessage.length;index++){
-      const code = criptoMessage.charAt(index).charCodeAt();
-      //quiere decir que es una mayuscula
-      if(code > 64 && code < 91){
-        var newOffSet = parseInt(offSet);
-        var newCode = code - newOffSet;
-        while(newCode < 65){
-          //calcula el desface con respecto al principio del alfabedo para descontar despues al final.
-          newCode =  (newCode - 64) + 90;
-          newOffSet = newOffSet - 90;
-        }
-        result += String.fromCharCode(newCode);
-      }else if(code > 96 && code < 123){
-        var newOffSet = parseInt(offSet);
-        var newCode = code - newOffSet;
-        while(newCode < 97){
-          //calcula el desface con respecto al principio del alfabedo para descontar despues al final.
-          newCode =  (newCode - 96) + 122;
-          newOffSet -= 122;
-        }
-        result += String.fromCharCode(newCode);
-      }else if(code == 126){
-        result += " ";
-      }else{
-        //por mientras ~,para ver que hago con caracteres distintos.
-        code = 126;
-      }
-    }
-    return result;	
-  }
-}
-*/
 window.cipher = {
   encode: (message, offSet) => { 
-     //se declara un string con el alfabeto deseado
      const alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ abcdefghijklmnñopqrstuvwxyz1234567890áéíóú.,";
      let result = "";
-	 //se recorre letra a letra el mensaje
-     for(let i=0;i<message.length;i++){
-	   //se obtiene la letra, y se busca e el alfabeto y se obtiene el indice 
-       const index = alphabet.indexOf(message.charAt(i), 0);
-	   //si encuentra la letra 
-       if(index != -1){
-		 //suma al indice obtenido el desplazamiento (offset)
-         let newIndex  = index + parseInt(offSet);
-		 //si el indice nuevo es mayor al largo del alfabeto 
-         if(newIndex > alphabet.length)
-         {
-		   //obtiene el factor representativo del largo total del desplazamiento en el alfabeto.
-           let factor = newIndex / alphabet.length;
-		   //multiplica el fator por el largo del alfabeto y obtiene un indice que este entre los limites del alfabeto
-           newIndex -= parseInt(factor) * alphabet.length;
+     for(let i=0;i<message.length;i++){//Recorre mensaje caracter por caracter. 
+       const index = alphabet.indexOf(message.charAt(i), 0);//Obtengo caracter,busca en el "String" y obtengo Indice.
+       if(index != -1){ //Si... encuentra el caracter... 
+         let newIndex  = index + parseInt(offSet);//Suma indice al desplazamiento (offset), obteniendo nuevo indice.
+		     if(newIndex > alphabet.length){//Si... el nuevo indice es mayor al largo del "String"
+           let factor = newIndex / alphabet.length;//Obtiene cuantos "String" caben entre el indice y el desplazamiento.
+		       newIndex -= parseInt(factor) * alphabet.length;//Le resta al nuevo indice la cantidad de "String" obtenidos.
          }
-		 //concatena la letra obtenida al resultado.
-         result += alphabet[newIndex];
-       }else{
-		//sino encuentra la letra concatena ~   
-        result += "~";
+         result += alphabet[newIndex];//Concatena la letra obtenida al resultado.
+       }else{ //Si no encuentra el caracter, agregar ~
+		     result += "~"; 
        }
      }
-	 //retorna el resultado obtenido
      return result;	
    },
+
    decode: (criptoMessage,offSet) => {
-	 //se declara un string con el alfabeto deseado
      const alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ abcdefghijklmnñopqrstuvwxyz1234567890áéíóú.,";
      let result = "";
-	  //se recorre letra a letra el mensaje
-     for(let i=0;i<criptoMessage.length;i++){
-	   //se obtiene la letra, y se busca e el alfabeto y se obtiene el indice 
-       const index = alphabet.indexOf(criptoMessage.charAt(i), 0);
-       //si no encuentra la letra 
-	   if(index == -1){
-		  //concatena la letra ~ 
-          result += "~";
-       }else{
-		 //resta al indice obtenido el desplazamiento (offset)
-         let newIndex  = index - parseInt(offSet);
-		 //si el nuevo indice es menor que cero
-         if(newIndex < 0)
-         {
-		   //reinicia el nuevo indice a 0	 
-           newIndex = 0;
-		   //determina el desface que existe entre el principio del alfabeto y el indice obtenido
-           let desface = (parseInt(offSet) - index)
-		   //obtiene la cantidad de alfabetos que existen entre el desface y el offset
-           let bloques = parseInt(desface / alphabet.length);
-           //resta los bloques al desface y obtiene un rango valido entre los limites del alfabeto
-		   let resto = desface - (bloques * alphabet.length);
-		   //como nos estamos devolviendo obtiene el largo del alfabeto y resta el resto obtenido anteriormente.
-           newIndex = alphabet.length - resto;
+     for(let i=0;i<criptoMessage.length;i++){//Recorre mensaje caracter por caracter. 
+       const index = alphabet.indexOf(criptoMessage.charAt(i), 0);//Obtengo caracter,busca en el "String" y obtengo Indice.
+	     if(index != -1){//Si... encuentra el caracter... 
+         let newIndex  = index - parseInt(offSet);//Resta indice al desplazamiento (offset), obteniendo nuevo indice.
+         if(newIndex < 0){//Si... el nuevo indice es menor a cero. 
+           newIndex = 0;//Se asigna 0 al nuevo indice.
+           let desface = (parseInt(offSet) - index)//Resta el desface entre el indice y cero al offset.
+           let bloques = parseInt(desface / alphabet.length);//Obtiene cantidad de "String" entre desface y 0.
+		       let resto = desface - (bloques * alphabet.length);//Resta al desface la cantidad de "String" obtenido.
+           newIndex = alphabet.length - resto;//Obtiene el indice restanto el "resto" al largo del "String"
          }
-		 //concatena al resultado la letra obtenida
-         result += alphabet[newIndex];
+         result += alphabet[newIndex];//Concatena la letra obtenida al resultado.
+       }else{
+         result += "~";//Si no encuentra el caracter, agregar ~
        }
      }
-	 //retorna la palabra obtenida
      return result;	
-   }
- }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,75 +1,78 @@
   function clickCifrar(){
-	  if(isValid("cifrar")){
-      //obtengo el offset
-      const offSet = document.getElementById("offSetGo").value;
-      //obtengo el mensaje
-      const message =  document.getElementById("getMessage").value;
-      //Utilizo la libreria cipher para codificar el mensaje
-      const messageEncode = cipher.encode(message,offSet);
-      //El mensaje encriptado lo muestro en el elemento "showMessage"
-      document.getElementById("showMessage").value = messageEncode;
+	  if(Valid("cifrar")){ //Ejecuta función de validación para cifrar.
+      const offSet = document.getElementById("offSetGo").value; //Obtengo valor de Offset
+      const message =  document.getElementById("getMessage").value; //Obtengo Mensaje
+      const messageEncode = cipher.encode(message,offSet); //Envia paramentros a cipher.encode (Para codificarlo)
+      document.getElementById("showMessage").value = messageEncode; //Mensaje Codificado se despliega en ("showMessage")
 	  }
   }
   
   function clickDecifrar(){
-    if(isValid("decifrar")){
-      //obtengo el offset
-      const offSet = document.getElementById("offSetGo").value;
-      //obtengo el mensaje
-      const message =  document.getElementById("showMessage").value;
-      //utilizo la libreria cipher para decodificar el mensaje
-      const messageDecode = cipher.decode(message,offSet);
-      //El mensaje encriptado lo muestro en el elemento "getMessage"
-      document.getElementById("getMessage").value = messageDecode;
-	}
-  
+    if(Valid("decifrar")){ //Ejecuta función de validación para Decifrar.
+      const offSet = document.getElementById("offSetGo").value; //Obtengo valor de Offset
+      const message =  document.getElementById("showMessage").value; //Obtengo Mensaje
+      const messageDecode = cipher.decode(message,offSet); //Envia paramentros a cipher.decode (Para decodificarlo)
+      document.getElementById("getMessage").value = messageDecode;//Mensaje Decodificado se despliega en ("getMessage")
+	  }
   }
-  //valida que exista contenido en los cuadros de texto.
-  function isValid(opcion){
-    let valid = true;
+
+  
+  function Valid(opcion){//Valida que exista contenido en los cuadros de texto.
+    let isValid = true;//Asigno valor por defecto.(Si entra en una retornara False)
     if (document.getElementById("offSetGo").value == ""){
-		document.getElementById("msgErrorOffSet").style.display = "block";
-      valid = false;
+		  document.getElementById("msgErrorOffSet").style.display = "block";
+      isValid = false;
     }else{
-		document.getElementById("msgErrorOffSet").style.display = "none";
-	}
-    switch (opcion) {
+		  document.getElementById("msgErrorOffSet").style.display = "none";
+	  }
+    switch (opcion) { //Selecciona la opción (Cifrar o Decifrar) y ejecuta bloque correspondiente.
       case "cifrar":
         if(document.getElementById("getMessage").value==""){
-			document.getElementById("msgErrorCifrar").style.display = "block";
-          valid =  false;
+			    document.getElementById("msgErrorCifrar").style.display = "block";
+          isValid =  false;
         }else{
-			document.getElementById("msgErrorCifrar").style.display = "none";
-		}
+			    document.getElementById("msgErrorCifrar").style.display = "none";
+		    }
         break;
       case "decifrar":
         if(document.getElementById("showMessage").value==""){
-			document.getElementById("msgErrorDecifrar").style.display = "block";
-          valid =  false;
+			    document.getElementById("msgErrorDecifrar").style.display = "block";
+          isValid =  false;
         }else{
-			document.getElementById("msgErrorDecifrar").style.display = "none";
-		}
-      break;
+			    document.getElementById("msgErrorDecifrar").style.display = "none";
+		    }
+        break;
     }
-    return valid;
+    return isValid;
   }
 
-  function clickClear(){
-    //limpio todo
+  function clickClear(){ //Botón para limpiar.
     document.getElementById("offSetGo").value = "";
     document.getElementById("showMessage").value = "";
     document.getElementById("getMessage").value = "";
-  
+    document.getElementById("msgErrorDecifrar").style.display = "none";
+    document.getElementById("msgErrorCifrar").style.display = "none";
+    document.getElementById("msgErrorOffSet").style.display = "none";
   }
 
-  function copyCifrar(){
+  function copyCifrar(){ //Al hacer click en el botón ejecuta esta función para copiar contenido Cifrado.
     const encodeText =  document.getElementById("getMessage");
-    encodeText.select();
+    encodeText.select();//Selecciona el contenido del elemento.
     document.execCommand("copy");
   }
 
-  function copyDesifrar(){
+  function copyDesifrar(){ //Al hacer click en el botón ejecuta esta función para copiar contenido Decifrado.
     const encodeText =  document.getElementById("showMessage");
     encodeText.select();
     document.execCommand("copy");
+  }
+
+  function closeHelp(){//Al hacer click en el botón cierra el panel de Ayuda.
+    document.getElementById("helpContent").style.display = "none";
+    document.getElementById("btnHelpMe").style.display = "block";
+  }
+
+  function openHelp(){//Al hacer click en el botón abre el panel de Ayuda.
+    document.getElementById("helpContent").style.display = "block";
+    document.getElementById("btnHelpMe").style.display = "none";
   }
