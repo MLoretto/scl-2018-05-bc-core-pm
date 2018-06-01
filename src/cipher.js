58d0baa98307bb39580fd491c884b77545d301c2
MLoretto <loreto.henriquez.m@gmail.com>
2018-06-01 15:03:01 -0400

    Ultimos avancesy modificaciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,59 @@
 window.cipher = {
+  encode: (message, offSet ) => { //Codifica
+    let result = "";
+    for(let index=0;index<message.length;index++){//Recorre mensaje letra por letra.
+      const code = message.charAt(index).charCodeAt();//Toma el caracter del mensaje y lo transforma en código ascii.
+      if(code > 64 && code < 91){//Rango de ascii Mayúsculas
+        var newCode = code + parseInt(offSet);//Suma desplazamiento al valor númerico de el caracter ascii.
+        while(newCode > 90){//Mientras el valor númerico de newCode sea mayor a 90 ira restando 26(Largo alfabeto)
+          newCode -= 26; 
+        }
+      }else if(code > 96 && code < 123){//Rango de ascii Minúsculas
+        var newCode = code + parseInt(offSet);//Suma desplazamiento al valor númerico de el caracter ascii.
+        while(newCode > 122){//Mientras el valor númerico de newCode sea mayor a 122 ira restando 26(Largo alfabeto)
+          newCode -= 26; 
+        }
+      }else{// Si no lo encuentra asignara caracter 126 código ascii ~
+        newCode = 126;
+      }
+      result += String.fromCharCode(newCode);//Transforma el código ascii a letra y lo concatena a resultado.
+    }
+	  return result;	
+  },
+  decode: (criptoMessage,offSet) => { //Decofica
+    let result = "";
+    for(let index=0;index<criptoMessage.length;index++){//Recorre mensaje letra por letra.
+      const code = criptoMessage.charAt(index).charCodeAt();//Toma el caracter del mensaje y lo transforma en código ascii.
+      if(code > 64 && code < 91){//Rango de ascii Mayúsculas
+        var newOffSet = parseInt(offSet);//Copia el offSet
+        var newCode = code - newOffSet;//Resta el desplasamiento (offset) al código ascii obtenido.
+        while(newCode < 65){//Mientras el valor númerico de newCode sea menor a 65 ira restando 64(Largo alfabeto)
+          newCode =  (newCode - 64) + 90;//Resta el desfase al rango ascii.
+          newOffSet = newOffSet - 90;//Obtiene el nuevo offset al restar el código de la última letra de ascii.
+        }
+        result += String.fromCharCode(newCode);//Transforma el código ascii a letra y lo concatena a resultado.
+      }else if(code > 96 && code < 123){//Rango de ascii Minúsculas
+        var newOffSet = parseInt(offSet);//Copia el offSet
+        var newCode = code - newOffSet;//resta el desplasamiento (offset) al código ascii obtenido
+        while(newCode < 97){//Mientras este fuera del rango de minúsculas del código Ascii
+          newCode =  (newCode - 96) + 122;//Resta el desfase al rango ascii
+          newOffSet -= 122;//Obtiene el nuevo offset al restar el código de la última letra de ascii.
+        }
+        result += String.fromCharCode(newCode);//Transforma el código ascii a letra y lo concatena a resultado.
+      }else if(code == 126){//Si encuentra un ~ le asigna un espacio.
+        result += " ";
+      }else{//Si no encuentra el caracter en el rango de mayusculas o minusculas le asigna un ~
+        code = 126;
+      }
+    }
+    return result;	
+  }
+}
+
+/* ESTA OPCION NO PASO LOS TEST :(
+window.cipher = {
   encode: (message, offSet) => { 
-     const alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ abcdefghijklmnñopqrstuvwxyz1234567890áéíóú.,";
+     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";// abcdefghijklmnñopqrstuvwxyz1234567890áéíóú.,";
      let result = "";
      for(let i=0;i<message.length;i++){//Recorre mensaje caracter por caracter. 
        const index = alphabet.indexOf(message.charAt(i), 0);//Obtengo caracter,busca en el "String" y obtengo Indice.
@@ -19,7 +72,7 @@ window.cipher = {
    },
 
    decode: (criptoMessage,offSet) => {
-     const alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ abcdefghijklmnñopqrstuvwxyz1234567890áéíóú.,";
+     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";// abcdefghijklmnñopqrstuvwxyz1234567890áéíóú.,";
      let result = "";
      for(let i=0;i<criptoMessage.length;i++){//Recorre mensaje caracter por caracter. 
        const index = alphabet.indexOf(criptoMessage.charAt(i), 0);//Obtengo caracter,busca en el "String" y obtengo Indice.
@@ -40,3 +93,4 @@ window.cipher = {
      return result;	
   }
 }
+*/
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -17,12 +17,12 @@ describe('cipher', () => {
 
     //Se realiza test para minisculas.
     it('debería retornar "defghijklmnopqrstuvwxyzabc" para "abcdefghijklmnopqrstuvwxyz" con offset 55', () => {
-      assert.equal(cipher.encode('abcdefghijklmnopqrstuvwxyz', 55),'defghijklmnopqrstuvwxyzabc');
+      assert.equal(cipher.encode('abcdefghijklmnopqrstuvwxyz',55),'defghijklmnopqrstuvwxyzabc');
     });
 
     //Se realiza un test combinado.
-    it('debería retornar "dEfGhIjKlMnOpQrStUvWxYzAbC" para "AbCdEfGhIjKlMnOpQrStUvWxYz" con offset 55', () => {
-      assert.equal(cipher.encode('AbCdEfGhIjKlMnOpQrStUvWxYz', 55),'dEfGhIjKlMnOpQrStUvWxYzAbC');
+    it('debería retornar "DeFgHiJkLmNoPqRsTuVwXyZaBc" para "AbCdEfGhIjKlMnOpQrStUvWxYz" con offset 55', () => {
+      assert.equal(cipher.encode('AbCdEfGhIjKlMnOpQrStUvWxYz', 55),'DeFgHiJkLmNoPqRsTuVwXyZaBc');
     });
   });
 
@@ -38,13 +38,65 @@ describe('cipher', () => {
 
     //Se realiza test para minisculas.
     it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "defghijklmnopqrstuvwxyzabc" con offset 55', () => {
-      assert.equal(cipher.encode('defghijklmnopqrstuvwxyzabc', 55),'abcdefghijklmnopqrstuvwxyz');
+      assert.equal(cipher.decode('defghijklmnopqrstuvwxyzabc', 55),'abcdefghijklmnopqrstuvwxyz');
     });
     
     //Se realiza un test combinado.
-    it('debería retornar "AbCdEfGhIjKlMnOpQrStUvWxYz" para "dEfGhIjKlMnOpQrStUvWxYzAbC" con offset 55', () => {
-      assert.equal(cipher.encode('dEfGhIjKlMnOpQrStUvWxYzAbC', 55),'AbCdEfGhIjKlMnOpQrStUvWxYz');
+    it('debería retornar "AbCdEfGhIjKlMnOpQrStUvWxYz" para "DeFgHiJkLmNoPqRsTuVwXyZaBc" con offset 55', () => {
+      assert.equal(cipher.decode('DeFgHiJkLmNoPqRsTuVwXyZaBc', 55),'AbCdEfGhIjKlMnOpQrStUvWxYz');
     });
   });
 
 });
+
+/* NO FUNCIONO :(
+describe('cipher', () => {
+
+  it('debería ser un objeto', () => {
+    assert.equal(typeof cipher, 'object');
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+  
+    it('debería retornar "fghijklmnñopqrstuvwxyz12345" para "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ" con offset 33', () => {
+      assert.equal(cipher.encode('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 33),'fghijklmnñopqrstuvwxyz12345');
+    });
+
+    //Se realiza test para minisculas.
+    it('debería retornar "234567890áéíóú.,ABCDEFGHIJK" para "abcdefghijklmnñopqrstuvwxyz" con offset 100', () => {
+      assert.equal(cipher.encode('abcdefghijklmnñopqrstuvwxyz', 100),'234567890áéíóú.,ABCDEFGHIJK');
+    });
+
+    //Se realiza un test combinado.
+    it('debería retornar "V3quzrs37qváqcrx5Nwzt7" para "El diablo es Magnífico" con offset 666', () => {
+      assert.equal(cipher.encode('El diablo es Magnífico', 666),'V3quzrs37qváqcrx5Nwzt7');
+    });
+  });
+
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+  
+    it('debería retornar "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ" para "fghijklmnñopqrstuvwxyz12345" con offset 33', () => {
+      assert.equal(cipher.decode('fghijklmnñopqrstuvwxyz12345', 33),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+
+    //Se realiza test para minisculas.
+    it('debería retornar "abcdefghijklmnñopqrstuvwxyz" para "234567890áéíóú.,ABCDEFGHIJK" con offset 100', () => {
+      assert.equal(cipher.encode('234567890áéíóú.,ABCDEFGHIJK', 100),'abcdefghijklmnñopqrstuvwxyz');
+    });
+    
+    //Se realiza un test combinado.
+    it('debería retornar "El diablo es Magnífico" para "V3quzrs37qváqcrx5Nwzt7" con offset 666', () => {
+      assert.equal(cipher.encode('V3quzrs37qváqcrx5Nwzt7', 666),'El diablo es Magnífico');
+    });
+  });
+
+});
+*/
